10202cd380467390e50ffd4eddf7379d438daa0b
Corey Quillen <corey.quillen@gmail.com>
2020-12-06 08:21:17 -0600

    try reading image from file system

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { SafeAreaView, StatusBar, Button, Image } from 'react-native';
 import CameraRoll from "@react-native-community/cameraroll";
+import RNFS from 'react-native-fs';
 
 class App extends Component {
   constructor(props) {
@@ -12,13 +13,22 @@ class App extends Component {
   }
 
   async getPhotos() {
-    var res = await CameraRoll.getPhotos({
-      first: 1
-    });
-    console.log('CameraRoll.getPhotos() response', res);
-    this.setState({
-      imageUrl: res.edges[0].node.image.uri
-    });
+    try {
+      var res = await CameraRoll.getPhotos({
+        first: 1
+      });
+      var imageUrl = res.edges[0].node.image.uri;
+
+      this.setState({
+        imageUrl: imageUrl
+      });
+
+      var imageBase64 = await RNFS.readFile(imageUrl, 'base64');
+      console.log(imageBase64);
+    }
+    catch (error) {
+      console.log(error);
+    }
   }
 
   render() {
@@ -28,7 +38,7 @@ class App extends Component {
         <SafeAreaView>
           <Button
             onPress={this.getPhotos}
-            title="Pick an image"
+            title="Get latest photo"
           />
           { this.state.imageUrl &&
             <Image
